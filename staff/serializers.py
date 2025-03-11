eefa8145e2742c993fe7ceb480af88147b876551
keith <keithmwanandimai@gmail.com>
2025-03-11 18:51:52 +0000

    failied to fix editing images

--- staff/serializers.py
+++ staff/serializers.py
@@ -29,6 +29,9 @@ class VehicleImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = VehicleImage
         fields = ["id", "image"]
+        extra_kwargs = {
+            "image": {"use_url": True}  # This ensures the URL is included in the response
+        }
 
 class VehicleSerializer(serializers.ModelSerializer):
     images = VehicleImageSerializer(many=True, read_only=True)
@@ -37,17 +40,35 @@ class VehicleSerializer(serializers.ModelSerializer):
         write_only=True,
         required=False
     )
+    removed_images = serializers.ListField(
+        child=serializers.IntegerField(),
+        write_only=True,
+        required=False
+    )
 
     class Meta:
         model = Vehicle
-        fields = ["id", "make", "model", "manufacture_year", "color", "mileage", "mileage_allowance", "ownership", "price_per_day", "deposit", "maintenance_records", "registration_number", "next_service_date", "images", "image_uploads"]
+        fields = ["id", "make", "model", "manufacture_year", "color", "mileage", "mileage_allowance", "ownership", "price_per_day", "deposit", "maintenance_records", "registration_number", "next_service_date", "images", "image_uploads", "removed_images"]
 
-    def create(self, validated_data):
-        images_data = validated_data.pop("image_uploads", [])
-        vehicle = Vehicle.objects.create(**validated_data)
-        for image_data in images_data:
-            VehicleImage.objects.create(vehicle=vehicle, image=image_data)
-        return vehicle
+    def update(self, instance, validated_data):
+        images_data = validated_data.pop("image_uploads", None)
+        removed_images = validated_data.pop("removed_images", [])
+
+        # Update other fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        # Handle new image uploads
+        if images_data:
+            for image_data in images_data:
+                VehicleImage.objects.create(vehicle=instance, image=image_data)
+
+        # Remove deleted images
+        if removed_images:
+            VehicleImage.objects.filter(id__in=removed_images).delete()
+
+        return instance
 
 
 class VehicleUnavailabilitySerializer(serializers.ModelSerializer):
