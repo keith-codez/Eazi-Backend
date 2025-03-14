c7ea048444b0a5de718f85223606987245bccc87
keith <mwanazk@gmail.com>
2025-03-14 23:16:38 +0000

    added search and all cars

--- staff/serializers.py
+++ staff/serializers.py
@@ -48,25 +48,38 @@ class VehicleSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Vehicle
-        fields = ["id", "make", "model", "manufacture_year", "color", "mileage", "mileage_allowance", "ownership", "price_per_day", "deposit", "maintenance_records", "registration_number", "next_service_date", "images", "image_uploads", "removed_images"]
+        fields = ["id", "make", "model", "manufacture_year", "color", "mileage", "mileage_allowance", "ownership", 
+                  "price_per_day", "deposit", "maintenance_records", "registration_number", "next_service_date", 
+                  "images", "image_uploads", "removed_images"]
+
+    def create(self, validated_data):
+        """ Handles creating a vehicle along with image uploads. """
+        images_data = validated_data.pop("image_uploads", [])  # Extract image data before creation
+        vehicle = Vehicle.objects.create(**validated_data)  # Create vehicle
+        
+        # Handle image uploads
+        for image_data in images_data:
+            VehicleImage.objects.create(vehicle=vehicle, image=image_data)
+        
+        return vehicle
 
     def update(self, instance, validated_data):
-        images_data = validated_data.pop("image_uploads", None)
-        removed_images = validated_data.pop("removed_images", [])
+        """ Handles updating a vehicle, including adding/removing images. """
+        images_data = validated_data.pop("image_uploads", [])  # Extract images
+        removed_images = validated_data.pop("removed_images", [])  # Extract IDs of images to remove
 
-        # Update other fields
+        # Update vehicle fields
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         instance.save()
 
         # Handle new image uploads
-        if images_data:
-            for image_data in images_data:
-                VehicleImage.objects.create(vehicle=instance, image=image_data)
+        for image_data in images_data:
+            VehicleImage.objects.create(vehicle=instance, image=image_data)
 
-        # Remove deleted images
+        # Handle image deletions
         if removed_images:
-            VehicleImage.objects.filter(id__in=removed_images).delete()
+            VehicleImage.objects.filter(id__in=removed_images, vehicle=instance).delete()
 
         return instance
 
